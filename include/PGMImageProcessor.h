231342d955bf68ad18247b0eb19be1430b3ed4ca
Zidaan <zizouhabib@gmail.com>
2021-04-22 00:49:46 +0200

    Added list of unique pointers to ConnectedComponents as a member of PGMImageProcessor

--- include/PGMImageProcessor.h
+++ include/PGMImageProcessor.h
@@ -1,12 +1,17 @@
 #ifndef PGMIP_H
 #define PGMIP_H
 #include <string>
+#include <memory>
+#include <list>
+#include "ConnectedComponent.h"
 
 class PGMImageProcessor{
 
     private:
         int rows, cols;
         unsigned char ** image;
+        std::list<std::unique_ptr<ConnectedComponent> > components;
+
     public: 
         PGMImageProcessor(std::string filename); //Custom constructor
         ~PGMImageProcessor(); // destructor
@@ -14,9 +19,10 @@ class PGMImageProcessor{
         PGMImageProcessor& operator=(const PGMImageProcessor & rhs); // copy assignment constructor
         PGMImageProcessor(PGMImageProcessor && p); // move constructor
         PGMImageProcessor& operator=(PGMImageProcessor && rhs); //move assignment operator
-        unsigned char** loadImage(std::string filename);
+        unsigned char** loadImage(std::string filename); 
         void writeImage(std::string filename,unsigned char ** image);
+        int extractComponents(unsigned char threshold);
     
 };
 
-#endif
+#endif  
