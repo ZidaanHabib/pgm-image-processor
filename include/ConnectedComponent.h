ed5d2327e81a85a92a4a1de60a9944c930e938bb
Zidaan <zizouhabib@gmail.com>
2021-04-21 14:10:03 +0200

    Added ConnectedComponent header file

---
+++ include/ConnectedComponent.h
@@ -0,0 +1,31 @@
+#ifndef CC_H
+#define CC_H 
+#include <vector>
+#include <utility>
+
+class ConnectedComponent {
+    private: 
+        friend class PGMImageProcessor;
+        int numPixels;
+        int id;
+        std::vector<std::pair<int, int> > pixels;
+
+        //Methods:
+        ConnectedComponent(); // default constructor    
+        ~ConnectedComponent(); // destructor
+        ConnectedComponent(const ConnectedComponent & c); // copy constructor
+        ConnectedComponent& operator=(const ConnectedComponent & rhs); // copy assignment operator
+        ConnectedComponent(ConnectedComponent && c); // move constructor
+        ConnectedComponent& operator=(ConnectedComponent && rhs); // move assignment operator
+
+
+
+
+        void addPixel(int x, int y);
+        
+};
+
+
+
+
+#endif
